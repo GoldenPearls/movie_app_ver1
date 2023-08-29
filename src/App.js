6466f46395766368c0f07fa05a6fb83ca986b082
[GoldenPearls] <prettylee620@naver.com>
2023-08-29 14:27:12 +0900

    [function]데이터 가져오기 기능 틀 추가 1. componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [], //영화데이터를 저장할 배열
+  };
+
+  getMovies = async () => {
+    // 액세스 실행
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+  };
+
+  componentDidMount() {
+    //영화 앱을 데이터를 불러올 것이다.
+    //setTimeout(() => {
+    //  this.setState({ isLoading: false });
+    //}, 6000); //6초후 We are Loding
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading } = this.state; //구조 분해 할당 : this.state를 입력하지 않아도 된다.
+    return <div>{isLoading ? "Loading.." : "영화 데이터를 출력"}</div>;
+  }
 }
 
 export default App;
