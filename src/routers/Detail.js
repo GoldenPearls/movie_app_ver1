d8c159328eeef94e721ad58e2d8247f939774606
[GoldenPearls] <prettylee620@naver.com>
2023-08-30 15:15:09 +0900

    [chore] 자잘한 스타일링 수정 및 깃허브 배포를 위한 주소 지정

--- src/routers/Detail.js
+++ src/routers/Detail.js
@@ -12,7 +12,18 @@ class Detail extends React.Component {
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <div className="container">
+          <img
+            src={location.state.poster}
+            alt={location.state.title}
+            title={location.state.title}
+          />
+          <h3 className="detail_title">{location.state.title}</h3>
+          <h4 className="detail_year">{location.state.year}</h4>
+          <h4 className="detail_summary">{location.state.summary}</h4>
+        </div>
+      );
     } else {
       return null;
     }
