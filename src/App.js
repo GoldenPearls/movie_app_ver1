bc1e275f1db192a38f8a09453bf033e180bf3fb0
[GoldenPearls] <prettylee620@naver.com>
2023-08-30 13:28:25 +0900

    [style] 라우터에 컴포넌트 추가 및 about 스타일 *app.js* 1. 라우터에 Home 컴포넌트를 메인으로 추가 *About.css* 1. css 스타일링

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import "./App.css";
 import About from "./routers/About";
+import Home from "./routers/Home";
 
 function App() {
   return (
     <HashRouter>
+      <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
   );
