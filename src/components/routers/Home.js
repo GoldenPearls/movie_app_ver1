41d7fdac9af86d30fd0bba61723603d58d6843a4
[GoldenPearls] <prettylee620@naver.com>
2023-08-30 11:35:51 +0900

    [style]Home.js로 옮기기 1. Home.js 작업 - app.js에서 있는 것을 가지고 오기 2. Home.css 작업 3. App.js를 Home.js로 연동하기

--- src/components/routers/Home.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import axios from "axios";
-import "../components/Movie";
-import "./Home.css";
-
-class Home extends React.Component {}
-
-export default Home;
---
+++ src/routers/Home.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React from "react";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [], //영화데이터를 저장할 배열
+  };
+
+  getMovies = async () => {
+    const {
+      // 데이터가 있는데 그 안에는 movies라는 데이터가 있음
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    ); // json 다음에 ?로 api 가져오는 방법
+    //console.log(movies);//movies.data.data.movies 정연산자 적용 => 구조 분해 할당하여 객체 구조화 ES6
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    //영화 앱을 데이터를 불러올 것이다.
+    //setTimeout(() => {
+    //  this.setState({ isLoading: false });
+    //}, 6000); //6초후 We are Loding
+    this.getMovies();
+  }
+
+  render() {
+    // 보여지는 부분
+    const { isLoading, movies } = this.state; //구조 분해 할당 : this.state를 입력하지 않아도 된다.
+    // 하나씩 받아오는 map
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
